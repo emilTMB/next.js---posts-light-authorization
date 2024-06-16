6a38826c40da261d56756b59cc10929ca91729ca
emilTMB <kvemil@yandex.ru>
2024-06-16 14:31:55 +0300

    polished

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = () => {
     return (
         <header className="container">
-            <Link href='/authorization'>Авторизация</Link>
+            <Link href='/'>Авторизация</Link>
         </header>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Login from "../components/login";
+import Login from "./components/login";
 import styles from "./page.module.scss";
 
 const LoginPage = () => {
